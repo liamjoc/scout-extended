671aa81d75be9e29bc11c149cd30b8e9966d4eb9
Devin Beeuwkes <devin.beeuwkes@algolia.com>
2021-09-20 14:36:49 +0200

    Update AggregatorTest.php

--- tests/Features/AggregatorTest.php
+++ tests/Features/AggregatorTest.php
@@ -282,6 +282,9 @@ final class AggregatorTest extends TestCase
 
         $threads = factory(Thread::class, 2)->create();
 
+        $threadIndexMock->shouldReceive('deleteBy')->once();
+        $wallIndexMock->shouldReceive('deleteBy')->once();
+
         config(['scout.queue' => true]);
         $this->assertEquals(0, app(Queue::class)->size($threads->first()->syncWithSearchUsingQueue()));
         $threads->unsearchable();
